908e6e5f877c6097102119638a264123ac003ae9
RodolfoIolo <Zby0000001@outlook.com>
2026-05-17 14:44:23 +0800

    fix:the User model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,11 +1,18 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model        //自动带入ID、CreatedAt、UpdatedAt、DeletedAt字段
-	Username   string `gorm:"type:varchar(100);uniqueIndex;not null" json:"username"`
-	Password   string `gorm:"type:varchar(255);not null" json:"-"`
-	Email      string `gorm:"type:varchar(100);uniqueIndex" json:"email"`
-	IsAdmin    bool   `gorm:"default:false" json:"is_admin"`
+	ID        uint           `gorm:"primaryKey;autoIncrement" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	Username  string         `gorm:"type:varchar(100);uniqueIndex;not null" json:"username"`
+	Password  string         `gorm:"type:varchar(255);not null" json:"-"`
+	Email     string         `gorm:"type:varchar(100);uniqueIndex" json:"email"`
+	IsAdmin   bool           `gorm:"default:false" json:"is_admin"`
 }
